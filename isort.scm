62c103611c12491ba41c4795588baedf0430f261
Richard Ding <roibdna@gmail.com>
2014-10-01 16:08:42 -0400

    use null?

--- isort.scm
+++ isort.scm
@@ -1,10 +1,11 @@
 ;;; insertion sort
 (define (isort xs)
-  (if (eqv? xs '()) 
+  (if (null? xs) 
       '()
       (insert (car xs) (isort (cdr xs)))))
 
 (define (insert elem xs)
-  (cond ((eqv? '() xs) (cons elem '()))
+  (cond ((null? xs) (cons elem '()))
         ((< elem (car xs)) (cons elem xs))
         (else (cons (car xs) (insert elem (cdr xs))))))
+
